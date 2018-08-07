b6bc7b35bb593b31a114dd38a07f46ccbe84d419
israelhz <israel.leal25@gmail.com>
2018-08-07 17:03:20 -0500

    Implements article show feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+
   def index
     @articles = Article.all
   end
@@ -25,5 +27,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
