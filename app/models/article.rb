33b95e72554555013ce6b09db8561d73c7ab2455
israelhz <israel.leal25@gmail.com>
2018-08-08 16:14:29 -0500

    Implements adding comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   validates :title, :body, presence: true
   default_scope { order(created_at: :desc) }
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
